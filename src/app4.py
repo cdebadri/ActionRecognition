92472f22854910f7e8b9fc7adadca9ce583095bd
cdebadri <cdebadri@gmail.com>
2019-05-05 00:56:11 +0530

    removed end frame error

--- src/app4.py
+++ src/app4.py
@@ -31,9 +31,9 @@ class App:
     def update(self):
         # Get a frame from the video source
         ret, frame = self.vid.get_frame()
-        frame = cv2.resize(frame, (640, 480))
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         if ret:
+            frame = cv2.resize(frame, (640, 480))
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
             self.C.create_image(0, 0, image = self.photo, anchor = tkinter.NW)
         if(self.count==0):
